262c72475442d76356f8b059356af5b9b38c9dda
Ronald Osure <rosure@kenet.or.ke>
2018-04-30 15:42:04 +0300

    break a test

--- polls/tests.py
+++ polls/tests.py
@@ -50,7 +50,7 @@ class QuestionIndexViewTests(TestCase):
         """
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No polls are available.")
+        self.assertContains(response, "No polls are available1.")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
 
     def test_past_question(self):
